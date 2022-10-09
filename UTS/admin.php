fc97993320f204aee322d79cbc11a7e5240dc90c
KaniaPutriAnanda <93080099+KaniaPutriAnanda@users.noreply.github.com>
2022-10-09 15:24:57 +0800

    Add files via upload

---
+++ UTS/admin.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="nucleo-icons.css?v3" rel="stylesheet" />
+  <link href="nucleo-svg.css?v2" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="nucleo-svg.css?v2" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="argon-dashboard.css?v=2.0.4" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+  <link href="style.css?v7" rel="stylesheet">
+</head>
+
+<body id="body">
+    <div><h1 class="teks">Login Admin Kacansg</h1></div>
+    <div class="cute2"><img class="cute" src="https://i.pinimg.com/564x/97/22/c8/9722c805fa7fc4aea9aac7c18e3171e6.jpg" alt="" width="400px"></div>
+    <div class="kotak1"> <p>Pastikan untuk menginputkan semua data dengan benar</p> </div>
+    <div class="kotak2"> <p>Selalu koordinasikan sebelum membuat keputusan</p> </div>
+    <div class="kotak3"> <p>Laporkan jika ada yang mencurigakan</p> </div>
+  <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-7 fixed-start ms-4 " id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href="admin.php" target="_blank">
+        <div class="teks2"><p>Kananda - Admin</p></div>
+      </a>
+    </div>
+
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="../pages/dashboard.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="input.php">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-bag-17 text-warning text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Tambah Produk</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="#">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-album-2 text-success text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Lihat Produk</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="#">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-bulb-61 text-danger text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Ubah Produk</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="#">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-basket text-primary text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Hapus Produk</span>
+          </a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link " href="index.php">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-lock-circle-open text-info text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Sign In</span>
+          </a>
+        </li>
