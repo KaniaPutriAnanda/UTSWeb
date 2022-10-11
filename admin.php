2abd8b6dfa4f8092ab82554dc6a4feae1cc953cd
KaniaPutriAnanda <93080099+KaniaPutriAnanda@users.noreply.github.com>
2022-10-11 11:30:02 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,11 +5,11 @@
    <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="nucleo-icons.css?v3" rel="stylesheet" />
-  <link href="nucleo-svg.css?v2" rel="stylesheet" />
+  <link href="nucleo-icons.css" rel="stylesheet" />
+  <link href="nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="nucleo-svg.css?v2" rel="stylesheet" />
+  <link href="nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="argon-dashboard.css?v=2.0.4" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
